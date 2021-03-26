d74966307a2f0f329166c94e1ef66a22d8ff8acc
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-26 13:55:21 +0000

    added indentations

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ let answer = 0
 document.getElementById('button1').addEventListener('click', multiply)
 
 // Now, I write out what function 'multiply' does.
-function multiply () {
+function multiply() {
   numbOne = document.getElementById('text1').value
   numbTwo = document.getElementById('text2').value
   numbOne = parseInt(numbOne)
   numbTwo = parseInt(numbTwo)
   if (numbOne < 0 || numbTwo < 0) {
     for (counter = 0; counter > numbOne; counter--) {
-    answer = answer - numbTwo
-  }
-  document.getElementById('results').innerHTML = answer
+      answer = answer - numbTwo
+    }
+    document.getElementById('results').innerHTML = answer
   } else if (numbOne > 0 || numbTwo > 0) {
     for (counter = 0; counter < numbOne; counter++) {
-    answer = answer + numbTwo
-  }
-  document.getElementById('results').innerHTML = answer
+      answer = answer + numbTwo
+    }
+    document.getElementById('results').innerHTML = answer
   }
 }
